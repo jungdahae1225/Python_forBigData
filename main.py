a60334a5e1e3663f2a04f52708dc65adca4e90f3
jungdahae1225 <jungdahae1225@gmail.com>
2021-10-20 23:18:39 +0900

    change data & algo

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-# HW2)) bigdata homework for excel
+# HW2)) 20190356 컴퓨터학과 정다해 bigdata homework for excel
+'''
+update 20211020 for
+'''
 
-# import openpyxl
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
 
@@ -30,52 +32,60 @@ while True:
 # --add total score to list--#
 score = []
 # print(sheet_ranges.cell(row=row2, column=7).value)
+row_i = 2
+while True:  # row_num 2~11
+    score.append(sheet_ranges.cell(row=row_i, column=7).value)
+    row_i += 1
+    if row_i is row_num:
+        break
 
-row_num = 2
-while row_num < 12:  # row_num 2~11
-    score.append(sheet_ranges.cell(row=row_num, column=7).value)
-    row_num += 1
+# score.reverse() //실수 적용이 안돼서 sorted 함수 사용.
 
-# score.append(sheet_ranges.cell(row=2, column=7).value)
-# score.append(sheet_ranges.cell(row=3, column=7).value)
-# score.append(sheet_ranges.cell(row=4, column=7).value)
-# score.append(sheet_ranges.cell(row=5, column=7).value)
-# score.append(sheet_ranges.cell(row=6, column=7).value)
-# score.append(sheet_ranges.cell(row=7, column=7).value)
-# score.append(sheet_ranges.cell(row=8, column=7).value)
-# score.append(sheet_ranges.cell(row=9, column=7).value)
-# score.append(sheet_ranges.cell(row=10, column=7).value)
-# score.append(sheet_ranges.cell(row=11, column=7).value)
+list1 = sorted(score, key=float, reverse=True)
+print(list1)
+print(len(list1))
+# --update grade-- #
+row_i = 2
 
-# --About rank--#
-rank = []
-cnt = 0
-# print(len(score))
+a = int(len(list1) * 0.3)
+a_plus = int(a * 0.5)
+a_zero = a - a_plus
 
-score.sort()
-# print(score)
+b = int((len(list1) * 0.7) - a)
+b_plus = int(b * 0.5)
+b_zero = b - b_plus
 
-# --update grade--#
-row_num = 2
-while row_num < 12:  # row_num 2~11
-    v = score.index(sheet_ranges.cell(row=row_num, column=7).value)
+c = int((len(list1)) - a - b)
+c_plus = int(c * 0.5)
+c_zero = c - c_plus
 
-    if v == 9:
+print(a_plus, a_zero, b_plus, b_zero, c_plus, c_zero)
+while True:  # row_num 2~11
+    v = list1.index(sheet_ranges.cell(row=row_i, column=7).value) + 1
+
+    if v <= a_plus:
         g = 'A+'
-    elif v >= 7:
+    elif v <= a_plus + a_zero:
         g = 'A0'
-    elif v >= 5:
+    elif v <= a + b_plus:
         g = 'B+'
-    elif v >= 3:
+    elif v <= a + b_plus + b_zero:
         g = 'B0'
-    elif v == 2:
+    elif v <= a + b + c_plus:
         g = 'C+'
     else:
         g = 'C0'
 
-    sheet_ranges.cell(row=row_num, column=8, value=g)
-    row_num += 1
+    sheet_ranges.cell(row=row_i, column=8, value=g)
+
+    if sheet_ranges.cell(row=row_i, column=7).value < 40:  # 40점 미만이면 무조건 F
+        sheet_ranges.cell(row=row_i, column=8, value='F')
+
+    row_i += 1
+    if row_i is row_num:
+        break
 
 # print(row_num)  # row num = 12
 
-wb.save(filename="output.xlsx")  # file_save
+wb.save(filename="student.xlsx")  # file_save_update
+wb.save(filename="output.xlsx")  # file_save for output
